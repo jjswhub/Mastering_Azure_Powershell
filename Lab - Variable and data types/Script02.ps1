9730522433b2f65ec6828025bb7b9d3aac4bd95e
jjswhub <jaywillsday@gmail.com>
2025-10-21 22:33:41 -0400

    modified Scriptf02.ps1, fixed error in defining variable, .GetType

--- Lab - Variable and data types/Script02.ps1
+++ Lab - Variable and data types/Script02.ps1
@@ -3,6 +3,17 @@ $y=20
 
 $x=10
 $y=20
-
 'The value of x is' + $x
 'The value of y is' + $y
+
+"The value of x is $x"
+"The value of y is $y"
+
+$z=$x+$y
+$z
+
+$CourseName="Azure PowerShell"
+"The name of the course is $CourseName"
+
+$X.GetType()
+$CourseName.GetType()
