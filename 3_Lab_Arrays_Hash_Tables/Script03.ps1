a1d1a914bb67822b3538bc8d50561d8eb59a4433
jjswhub <jaywillsday@gmail.com>
2025-10-22 18:00:55 -0400

    modified Script03.psi

---
+++ 3_Lab_Arrays_Hash_Tables/Script03.ps1
@@ -0,0 +1,17 @@
+$CouerseVideos= 'Introduction','Installation','Variables'
+
+$CouerseVideos
+
+$CourseNumbers=1,2,3
+$CourseNumbers
+
+$CouerseVideos_1=@(
+   'Introduction'
+   'Installation'
+   'Variables'
+    
+)
+
+$CouerseVideos_1
+
+$CouerseVideos_1[0]
